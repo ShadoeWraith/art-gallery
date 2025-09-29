e40a7ece3956932143946be9df4c976e9006a2b1
ShadoeWraith <shadoewraith99@gmail.com>
2025-09-29 01:58:54 -0700

    updated env variable usage

--- src/routes/api/proxy/[...path]/+server.ts
+++ src/routes/api/proxy/[...path]/+server.ts
@@ -1,10 +1,11 @@
-import { PUBLIC_API_BASE } from '$env/static/public';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export async function GET({ params, url }: { params: { path: string | string[] }; url: URL }) {
 	const pathSegments = Array.isArray(params.path) ? params.path : [params.path];
 	const query = url.searchParams.toString();
 
-	const target = `${PUBLIC_API_BASE}/${pathSegments.join('/')}${query ? `?${query}` : ''}`;
+	const target = `${process.env.API_BASE}/${pathSegments.join('/')}${query ? `?${query}` : ''}`;
 
 	const res = await fetch(target);
 	const data = await res.json();
