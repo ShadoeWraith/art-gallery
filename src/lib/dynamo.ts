311ad460699a62e18476b1db1b8db6b96e7c88a7
Shaun Kiszonas <shadoewraith99@gmail.com>
2025-12-07 18:16:01 -0800

    updated AWS keys

--- src/lib/dynamo.ts
+++ src/lib/dynamo.ts
@@ -1,9 +1,11 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
+import { VITE_DYNAMO_ACCESS_KEY, VITE_DYNAMO_SECRET_KEY } from '$env/static/private';
+
 export const client = new DynamoDBClient({
 	region: 'us-west-1',
 	credentials: {
-		accessKeyId: 'AKIAQM5Q6V76QQICFE7R',
-		secretAccessKey: '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'
+		accessKeyId: VITE_DYNAMO_ACCESS_KEY,
+		secretAccessKey: VITE_DYNAMO_SECRET_KEY
 	}
 });
